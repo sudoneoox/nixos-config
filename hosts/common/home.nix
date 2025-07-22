4330761aae0d4beb6b013735066e9ab1347ad1ed
Diego Coronado <diegoa2992@proton.me>
2025-07-22 17:34:34 -0500

    overlays

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -10,11 +10,8 @@
 
   nixpkgs = {
     overlays = [
-      outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.stable-packages
-      outputs.overlays.nur
-      outputs.overlays.nix-vscode-extensions
     ];
 
     config = {
