904886431a65b700f399822813f297a526e98d24
X0Laptop@NixOS <diegoa2992@proton.me>
2025-08-04 18:03:43 -0500

    git hyprshade

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 {
   arc-icon-theme = pkgs.callPackage ./icons/arc-icon-theme.nix { };
-  hyprshade-git = pkgs.callPackage ./hyprshade { };
+  hyprshade-git = pkgs.callPackage ./hyprshade-git { };
 }
