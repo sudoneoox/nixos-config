d89bfb63a0dbc3f353988ce5e766b475e471023b
X0Laptop@NixOS <diegoa2992@proton.me>
2025-08-06 01:05:30 -0500

    remove uneeded workspace binds

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -275,12 +275,6 @@
           "SUPER, 2, workspace, 2"
           "SUPER, 3, workspace, 3"
           "SUPER, 4, workspace, 4"
-          "SUPER, 5, workspace, 5"
-          "SUPER, 6, workspace, 6"
-          "SUPER, 7, workspace, 7"
-          "SUPER, 8, workspace, 8"
-          "SUPER, 9, workspace, 9"
-          "SUPER, 0, workspace, 10"
 
           # (CTRL + ALT + ARROWS)
           "CTRL ALT, right, workspace, e+1"
@@ -291,11 +285,6 @@
           "SUPER SHIFT, 2, movetoworkspace, 2"
           "SUPER SHIFT, 3, movetoworkspace, 3"
           "SUPER SHIFT, 4, movetoworkspace, 4"
-          "SUPER SHIFT, 5, movetoworkspace, 5"
-          "SUPER SHIFT, 6, movetoworkspace, 6"
-          "SUPER SHIFT, 7, movetoworkspace, 7"
-          "SUPER SHIFT, 8, movetoworkspace, 8"
-          "SUPER SHIFT, 9, movetoworkspace, 9"
 
           # Scroll through workspaces
           "SUPER, mouse_down, workspace, e+1"
