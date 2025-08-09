4ceb0ef4ce41cfeb6cca7e30ef76d84a3f7c09c0
X0Laptop@NixOS <diegoa2992@proton.me>
2025-08-09 15:02:33 -0500

    binds -> bind

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -1,18 +1,6 @@
 {host, lib, username, pkgs, ...}:
 {
 
-  imports = [
-    ./cursor
-    ./waybar
-    ./hyprlock
-    ./hyprpaper
-    ./hyprshade
-    ./dunst
-    ./wallust
-    ../rofi
-  ] 
-    ++ lib.optional (host == "X0NixOSLaptop") ./single-monitor.nix
-    ++ lib.optional (host == "X0NixOSDesktop") ./multi-monitor.nix;
 
 
   # Assets that plugins or scripts use
@@ -257,7 +245,7 @@
         };
       };
 
-      binds = [
+      bind = [
         # Application binds
         "SUPER, SPACE, exec, ${terminal}"
         "SUPER, D, exec, ${filemanager}"
@@ -364,4 +352,17 @@
       };
     };
   };
+
+  imports = [
+    ./cursor
+    ./waybar
+    ./hyprlock
+    ./hyprpaper
+    ./hyprshade
+    ./dunst
+    ./wallust
+    ../rofi
+  ]
+    ++ lib.optional (host == "X0NixOSLaptop") ./single-monitor.nix
+    ++ lib.optional (host == "X0NixOSDesktop") ./multi-monitor.nix;
 }
