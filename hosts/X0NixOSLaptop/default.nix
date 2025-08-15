c440e000f5dbc51fcb62509b75f7b6511aa07606
X0Laptop@NixOS <diegoa2992@proton.me>
2025-08-14 22:37:45 -0500

    substituters modification, wallust backend change

--- hosts/X0NixOSLaptop/default.nix
+++ hosts/X0NixOSLaptop/default.nix
@@ -63,22 +63,22 @@
     };
     tlp = {
       enable = true;
-      "settings".CPU_SCALING_GOVERNER_ON_AC = "performance";
-      "settings".CPU_SCALING_GOVERNER_ON_BAT = "powersave";
-      "settings".CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      "settings".CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      "settings".CPU_MIN_PERF_ON_AC = 0;
-      "settings".CPU_MAX_PERF_ON_AC = 100;
-      "settings".CPU_MIN_PERF_ON_BAT = 0;
-      "settings".CPU_MAX_PERF_ON_BAT = 20;
-      "settings".START_CHARGE_THRESH_BAT0 = 40;
+      settings = {
+        CPU_SCALING_GOVERNER_ON_AC = "performance";
+        CPU_SCALING_GOVERNER_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "balance_power";
+        CPU_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+      };
     };
     thermald.enable = true;
-  };
-
-  programs.coolercontrol = {
-    enable = true;
-    nvidiaSupport = true;
   };
 
   home-manager.users.${username} = {
--- modules/base/substituters.nix
+++ modules/base/substituters.nix
@@ -4,16 +4,18 @@
       "https://cache.nixos.org?priority=10"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
-      "https://anyrun.cachix.org"
       "https://hyprland.cachix.org"
       "https://nyx.chaotic.cx"
+      "https://nvf.cachix.org"
+      "https://neovim-flake.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
+      "neovim-flake.cachix.org-1:iyQ6lHFhnB5UkVpxhQqLJbneWBTzM8LBYOFPLNH4qZw="
     ];
   };
 }
