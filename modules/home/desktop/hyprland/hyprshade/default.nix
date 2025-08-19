2021a85374265c8ea1966f073a06f7f959d544d4
X0Laptop@NixOS <diegoa2992@proton.me>
2025-08-19 18:11:59 -0500

    path resolution

--- modules/home/desktop/hyprland/hyprshade/default.nix
+++ modules/home/desktop/hyprland/hyprshade/default.nix
@@ -2,7 +2,7 @@ let
   shaderDirPath = ./shaders;
   tomlConfigFile = ./hyprshade.toml;
 in {
-  xdg.configFile."/hypr/hyprshade.toml".source = tomlConfigFile;
+  xdg.configFile."hypr/hyprshade.toml".source = tomlConfigFile;
   xdg.configFile."hypr/shaders" = {
     source = shaderDirPath;
     recursive = true;
