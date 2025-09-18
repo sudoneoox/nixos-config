2da48290901759114cdc4b4e8e45d0f46fc9004c
diego <diegoa2992@proton.me>
2025-09-18 10:32:50 -0500

    uwsm remove

--- modules/system/desktop/hyprland/default.nix
+++ modules/system/desktop/hyprland/default.nix
@@ -10,7 +10,7 @@ in {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd '${pkgs.uwsm}/bin/uwsm start hyprland'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd hyprland";
         user = "${x.identity.username}";
       };
     };
