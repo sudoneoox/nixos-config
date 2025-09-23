9d9a0669a472cc1571408418bbaa6c7bb23a8c82
diego <diegoa2992@proton.me>
2025-09-23 03:30:46 +0000

    d

--- hosts/Arch-HM/arch-bootstrap/run.sh
+++ hosts/Arch-HM/arch-bootstrap/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo pacman -Syu --needed ansible git python
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 ansible-galaxy collection install community.general
-ansible-playbook -i inventory.ini site.yml
+ansible-playbook -i inventory.ini site.yml -K
 
 # Install lix using their provided helper script
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix -o lix-install.sh && chmod +x lix-install.sh && ./lix.install.sh
