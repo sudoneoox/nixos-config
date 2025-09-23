f4edeb3fa89a10479070e580652d9c190dcced3f
diego <diegoa2992@proton.me>
2025-09-23 12:41:57 -0500

    chore: bump inputs

--- hosts/Arch-HM/default.nix
+++ hosts/Arch-HM/default.nix
@@ -41,7 +41,6 @@ in {
 
   fonts.fontconfig.enable = true;
 
-  # Home identity/paths (x.derived.homeDir is fine if it’s generic)
   home = {
     username = x.identity.username;
     homeDirectory = x.derived.homeDir;
@@ -90,7 +89,6 @@ in {
         "https://nyx.chaotic.cx"
         "https://nvf.cachix.org"
         "https://neovim-nightly.cachix.org"
-        #NOTE: My own cachix
         "https://nixossudnox.cachix.org"
       ];
 
@@ -102,7 +100,6 @@ in {
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
         "neovim-nightly.cachix.org-1:feIoInHRevVEplgdZvQDjhp11kYASYCE2NGY9hNrwxY="
-        #NOTE: My own cachix
         "nixossudnox.cachix.org-1:ZwKSBOS8npDqpdX9cg7kMvEx5dOSYq05O69qm5O0mLg="
       ];
 
