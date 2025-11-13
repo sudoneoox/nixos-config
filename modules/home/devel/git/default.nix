6e6c20da1f8cd07f210ebb9aafe9a3332d2fd49b
diego <diegoa2992@proton.me>
2025-11-13 12:32:45 -0600

    chore: update flakes

--- modules/home/devel/git/default.nix
+++ modules/home/devel/git/default.nix
@@ -3,16 +3,18 @@
 in {
   programs.git = {
     enable = true;
-    userName = "${x.identity.username}";
-    userEmail = "${x.identity.email}";
-    signing = {
-      key = "${x.identity.sshKeyPath}";
-      signByDefault = true;
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-      commit.gpgSign = true;
-      gpg.format = "ssh";
+    settings = {
+      user.name = "${x.identity.username}";
+      user.email = "${x.identity.email}";
+      signing = {
+        key = "${x.identity.sshKeyPath}";
+        signByDefault = true;
+      };
+      extraConfig = {
+        init.defaultBranch = "main";
+        commit.gpgSign = true;
+        gpg.format = "ssh";
+      };
     };
   };
 }
