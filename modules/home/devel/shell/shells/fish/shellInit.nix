50cf06a7b7a31e3de63521e2476e866a3475d3f1
diego <diegoa2992@proton.me>
2025-12-17 17:22:18 -0600

    chore: bump inputs

--- modules/home/devel/shell/shells/fish/shellInit.nix
+++ modules/home/devel/shell/shells/fish/shellInit.nix
@@ -7,6 +7,7 @@
 in {
   programs.fish.shellInit =
     ''
+      set -Ux PATH /home/${x.identity.username}/.local/bin $PATH
       set -gx SOPS_AGE_KEY_FILE ${x.sopsPath}/key.txt
       set -gx SOPS_AGE_RECIPIENTS ${x.sopsPublicKey}
 
